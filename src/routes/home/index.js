df3f6de9e39155865e83565f8709109d0839247e
Oleg Shalygin <oshalygin@gmail.com>
2017-10-01 17:54:22 -0700

    Remove React Starter Kit ref

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import Home from './Home';
 import Layout from '../../components/Layout';
@@ -21,7 +12,7 @@ async function action({ fetch }) {
   if (!data || !data.news) throw new Error('Failed to load the news feed.');
   return {
     chunks: ['home'],
-    title: 'React Starter Kit',
+    title: 'Oleg Shalygin',
     component: (
       <Layout>
         <Home news={data.news} />
