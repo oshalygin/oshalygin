4a5f7d564a9af0848c2638130477c7c1f46ea468
Oleg Shalygin <oshalygin@gmail.com>
2017-10-01 18:40:03 -0700

    Refactor feedback component

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Feedback.css';
+import s from './feedback.css';
 
 class Feedback extends React.Component {
   render() {
